fbbf9160fbddc0cef34313483f257621a4bb18f1
Antonia <konto_326@tu-berlin.de>
2020-08-14 11:06:21 +0200

    Argparse defaults and help messages.

--- burndownchart.py
+++ burndownchart.py
@@ -13,20 +13,12 @@ sys.argv[3] : first day of sprint (mo, di, mi, do, fr, sa, so)
 sys.argv[4] : length of sprint in days
 """
 
-parser = argparse.ArgumentParser(description= "Burndownchart that optionally shows how much time actually went into the tasks of the sprint")
-
-parser.add_argument("data", help="Data file with issue number, planned time, day of completion, and - optionally - actually used time")
-parser.add_argument("graphs", type=int, help="Number of graphs to plot: either two (ideal and actual) or three (actual time)")
-parser.add_argument("firstdayofsprint", help="either mo, di, mi, do, fr, sa, or so")
-parser.add_argument("duration", help="Duration of sprint in days", type=int)
+parser = argparse.ArgumentParser(description= "Plots a burndownchart and optionally the time that actually went into the tasks of the sprint")
+parser.add_argument("data", help="Data file with issue number, planned time in minutes, day of completion, and - optionally - actually used time in minutes")
+parser.add_argument("graphs", type=int, nargs='?', default=2, help="Number of graphs to plot: either 2 (ideal and actual) or 3 (plus actual time)")
+parser.add_argument("firstdayofsprint", nargs='?', default="mo", help="Either mo, di, mi, do, fr, sa, or so")
+parser.add_argument("duration", type=int, nargs='?', default=7, help="Duration of sprint in days")
 args = parser.parse_args()
-
-
-if len(sys.argv) < 2:
-    print("Usage:\n\tpython3 <scriptname> <datafile> <graphs> <start> <duration>\n")
-    print("\te.g.\t python3 burndownchart3.py Sprint4 3 fr 14\n")
-if len(sys.argv) < 4:
-    sys.argv.extend(["2", "mo", "7"])
 
 def sprintweek(day):
     week = ["mo", "di", "mi", "do", "fr", "sa", "so"] 
@@ -54,9 +46,7 @@ def map_days(sprint, duration):
     sprintdays = dict(zip(sprint, daynum))
     return sprintdays
     
-#sprintweek = sprintweek(sys.argv[3])
 sprintweek = sprintweek(args.firstdayofsprint)
-#duration = int(sys.argv[4])
 duration = int(args.duration)
 sprint = adjust_sprintlength(duration, [], sprintweek)
 sprintdays = map_days(sprint, duration)
@@ -69,7 +59,6 @@ day_in_minutes = 480
 actualminutes = 0
 buffer_minutes = 0
 
-#with open (sys.argv[1]) as f:
 with open (args.data) as f:
     firstline = f.readline().split()
     columns = len(firstline)
@@ -82,7 +71,7 @@ with open (args.data) as f:
         minutes = int(minutes_str)
         sum_minutes += minutes
         issues.append(issue)
-        if issue.startswith("p"):
+        if issue.startswith("b"):
             buffer_minutes += minutes
         if day in sprintdays:
             burnt_minutes[sprintdays[day]] += minutes
@@ -109,7 +98,7 @@ with open (args.data) as f:
 
 print("sprint = ", sprint)
 unique_issues = list(set(issues))
-print("number of issues = ", len(issues), "number of unique issues = ", len(unique_issues))
+print("number of issues = \t", len(issues), "\nnumber of unique issues = ", len(unique_issues))
 if len(issues) != len(unique_issues):
     print("Duplicate issues in issue file!")
 print("average minutes/task = ", sum_minutes/len(issues))
@@ -131,8 +120,7 @@ def minutes2days(burnt_week):
 
 workload_week = [sum_minutes]* (duration+1)
 burntdown = burn_minutes(workload_week, burnt_minutes)
-burnt_days = minutes2days(burntdown)
-burnt_days_arr = np.array(burnt_days)
+burnt_days_arr = np.array(minutes2days(burntdown))
 
 tasks_in_days = sum_minutes/day_in_minutes
 
@@ -144,12 +132,11 @@ plt.xticks(x)
 if burnt_minutes != [0]*(duration+1):
     plt.plot(x, burnt_days_arr, label = "Actual Tasks Remaining", marker='o')
 
-if sys.argv[2] == "3":
+if args.graphs == 3:
     workload_week = [sum_minutes]*(duration+1)
     burntdown_actual = burn_minutes(workload_week, burnt_actualminutes)
     if burnt_actualminutes != [0]*(duration+1):
-        burnt_days_actual = minutes2days(burntdown_actual)
-        burnt_days_actual_arr = np.array(burnt_days_actual)
+        burnt_days_actual_arr = np.array(minutes2days(burntdown_actual))
         plt.plot(x, burnt_days_actual_arr, label = "Actual Time Burnt")
         if burntdown_actual[-1] >= 0:
             ax = plt.subplot(1,1,1)
@@ -160,7 +147,7 @@ if buffer_minutes != 0:
     plt.axhline(y=0, color='black', linewidth=0.5)
 
 ax = plt.subplot(1,1,1)
-if sys.argv[2] == "2":
+if args.graphs == 2:
     ax.set_ylim(ymin=0)
 ax.set_xlim(xmin=0)
 sprint.insert(0,"0")
@@ -175,7 +162,7 @@ ax.spines['top'].set_visible(False)
 ax.spines['left'].set_visible(True)
 
 plt.ylabel('Tasks (in days)\n' + str(np.round(sum_minutes/60,2)) + 'h = '+ str(np.round(sum_minutes/day_in_minutes, 2)) + ' days')
-filename = sys.argv[1].partition(".")
+filename = args.data.partition(".")
 plt.title('Burndown Chart ' + filename[0])
 plt.legend()
 plt.tight_layout()
